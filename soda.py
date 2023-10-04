b54259e8dbe5e2fb7828e40c219ea3d8d4e85a45
nuclear77 <spiritvoideu@gmail.com>
2023-10-04 21:35:22 +0300

    soda psadkapsdop

--- soda.py
+++ soda.py
@@ -14,45 +14,47 @@
 
 
 # задание 2
-# class TriangleChecker:
-#     def __init__(self, a, b, c):
-#         self.a = a
-#         self.b = b
-#         self.c = c
-#
-#     def is_triangle(self):
-#         if self.a + self.b >= self.c:
-#             print('треугольник можно построить')
-#         elif self.a or self.b or self.c < 0:
-#             print('с отрицательными нельзя')
-#         elif self.a or self.b or self.c == str:
-#             print('строку нельзя')
-#         else:
-#             self.a + self.b < self.c
-#             print('нельзя построить')
-#
-#
-# triangle1 = TriangleChecker(2, 3, 4)
-#
-# print(triangle1.is_triangle())
+class TriangleChecker:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def is_triangle(self):
+        if self.a + self.b >= self.c:
+            print('треугольник можно построить')
+        elif self.a or self.b or self.c < 0:
+            print('с отрицательными нельзя')
+        elif self.a or self.b or self.c == str:
+            print('строку нельзя')
+        else:
+            print('нельзя построить')
+
+
+triangle1 = TriangleChecker(2, 3, 4)
+
+print(triangle1.is_triangle())
 
 
 # задание 3
-class KgToPounds:
-    def __init__(self, kg):
-        self.__kg = kg
-    @property
-    def kg(self):
-        return self.__kg
-    @kg.setter
-    def kg(self, new_kg):
-        if isinstance(new_kg, (int, float)):
-            self.__kg = new_kg
-        else:
-            raise ValueError('килограммы задаются числами')
+# class KgToPounds:
+#     def __init__(self, kg):
+#         self.__kg = kg
+#
+#     @property
+#     def kg(self):
+#         return self.__kg
+#
+#     @kg.setter
+#     def kg(self, new_kg):
+#         if isinstance(new_kg, (int, float)):
+#             self.__kg = new_kg
+#         else:
+#             raise ValueError('килограммы задаются числами')
+#
+#     def to_pounds(self):
+#         return self.__kg * 2.205
 
-    def to_pounds(self):
-        return self.__kg * 2.205
 
 kgtopd1 = KgToPounds(10)
 print(kgtopd1.kg)
