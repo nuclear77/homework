c97e7c32603d08584318556ad5968c927ef9c6b9
nuclear77 <spiritvoideu@gmail.com>
2023-10-07 18:49:21 +0300

    completed homework 9

---
+++ hw9.py
@@ -0,0 +1,28 @@
+class DataClass:
+    def __init__(self, data):
+        self.data = data
+
+    @staticmethod
+    def static_method():
+        print("это статикметод")
+
+    @classmethod
+    def class_method(cls):
+        print("это классметод")
+
+
+DataClass.static_method()
+
+DataClass.class_method()
+
+class MetaClass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['new_attribute'] = "добавление нового аттрибута в метакласс"
+        return super().__new__(cls, name, bases, attrs)
+
+class MyClass(metaclass=MetaClass):
+    pass
+
+obj2 = MyClass()
+
+print(obj2.new_attribute)
