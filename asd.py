a73400a428c345107baef968d6992ffdb877fb93
nuclear7777 <spiritvoideu@gmail.com>
2023-10-17 18:57:00 +0300

    hw 16

--- asd.py
+++ asd.py
@@ -9,22 +9,6 @@ def index():
     color = generate_color()
     return render_template('index.html', color=color)
 
-@app.route('/page1')
-def page1():
-    color = generate_color()
-    return render_template('page1.html', color=color)
-
-@app.route('/page2')
-def page2():
-    color = generate_color()
-    return render_template('page2.html', color=color)
-
-@app.route('/page3')
-def page3():
-    color = generate_color()
-    return render_template('page3.html', color=color)
-
-
 def generate_color():
     colors = ['red', 'blue', 'green', 'yellow', 'purple']
     return random.choice(colors)
